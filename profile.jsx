eeaab497595e985c8c3ef2a6ead7a4fd6ce1eb18
sahelahmed <74127134+suhelahmedprojectspace@users.noreply.github.com>
2021-08-14 18:19:39 +0530

    Update profile.jsx

--- profile.jsx
+++ profile.jsx
@@ -3,33 +3,47 @@ import './Style/Profile.css';
 import profile from './Images/profile.jpg';
 import Navbar from './Navbar';
 import { NavLink } from 'react-router-dom';
+import PersonIcon from '@material-ui/icons/Person';
+import EmailIcon from '@material-ui/icons/Email';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import CakeIcon from '@material-ui/icons/Cake';
 const Profile = () => {
     return (
         <div className="profile_container">
             <Navbar />
             <div className="profile">
-                <img src={profile} alt="profile" />
+                <div className="profile_img">
+                    <img src={profile} alt="profile" />
+                </div>
+
                 <div className="profile_Card">
                     <div className="profile_info">
+                        <div className="profile_id">
+                            <PersonIcon /><h2>User Id:</h2><p>7825614544445</p>
+                        </div>
                         <div className="profile_name">
-                            <strong>Name:</strong><p>Suhel Ahmed</p>
+                            <AccountCircleIcon />  <h2>Name:</h2><p>Suhel Ahmed</p>
                         </div>
                         <div className="profile_email">
-                            <strong>Email:</strong><p>Suhel@gmail.com</p>
+                            <EmailIcon /><h2>Email:</h2><p>Suhel@gmail.com</p>
+                        </div>
+                        <div className="profile_dob">
+                            <CakeIcon /><h2>Birthday:</h2><p>20/9/1999</p>
                         </div>
 
                     </div>
 
-                    <div className="profile_dob">
-                        <form>
-                            <label for="birthday">Birthday:</label>
-                            <input type="date" id="birthday" name="birthday" />
-                        </form>
-                    </div>
-                    <div className="update">
-                        <NavLink to='/updateprofile' style={{ color: 'white', textDecoration: 'none' }}>Update</NavLink>
+                    <div className="profile_btn">
+                        <div className="update">
+                            <NavLink to='/updateprofile' style={{ color: 'black', textDecoration: 'none', letterSpacing: '2px' }}>Update</NavLink>
 
+                        </div>
+                        <div className="logout">
+                            <NavLink to='/logout' style={{ color: 'black', textDecoration: 'none' }}>Logout</NavLink>
+
+                        </div>
                     </div>
+
 
                 </div>
             </div>
