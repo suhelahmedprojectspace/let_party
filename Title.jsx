118c1a529a0104f68662ea0ef7a53792425fb482
sahelahmed <74127134+suhelahmedprojectspace@users.noreply.github.com>
2021-08-30 18:55:27 +0530

    Create Title.jsx

---
+++ Title.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Title = () => {
+    return (
+        <div className="title">
+            <h2>Your Pictures</h2>
+            <p>Click here to upload the image</p>
+        </div>
+    )
+}
+
+export default Title;
