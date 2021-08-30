b76c20fefd52daaafbc969e855d8c23a1fce4a7d
sahelahmed <74127134+suhelahmedprojectspace@users.noreply.github.com>
2021-08-30 18:54:07 +0530

    Create ImageGrid.jsx

---
+++ ImageGrid.jsx
@@ -0,0 +1,27 @@
+import { motion } from 'framer-motion';
+import useFirestore from './Hooks/UseFirestore';
+import './Style/UploadForm.css';
+
+const ImageGrid = ({ setSelectedImg }) => {
+    const { docs } = useFirestore('images');
+
+    return (
+        <div className="img-grid">
+            {docs && docs.map(doc => (
+                <motion.div className="img-wrap" key={doc.id}
+                    layout
+                    whileHover={{ opacity: 1 }} s
+                    onClick={() => setSelectedImg(doc.url)}
+                >
+                    <motion.img src={doc.url} alt="uploaded pic"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ delay: 1 }}
+                    />
+                </motion.div>
+            ))}
+        </div>
+    )
+}
+
+export default ImageGrid;
